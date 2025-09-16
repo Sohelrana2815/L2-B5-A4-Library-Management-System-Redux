4a7f4ab8fc01dd33e98d493770183695c4ea56e4
Sohelrana2815 <sohel152302@gmail.com>
2025-09-16 14:54:55 +0600

    done the website

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -8,7 +8,7 @@ interface ApiResponse<T> {
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://library-management-api-flame.vercel.app/api",
   }),
   tagTypes: ["books", "borrow"],
   endpoints: (builder) => ({
